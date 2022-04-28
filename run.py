ae3b9d3ed875b8ca7deb91410ab695e9a023948e
Mark Baggett <mbagget1@utk.edu>
2022-04-28 09:55:30 -0400

    Add arg parsing.

---
+++ run.py
@@ -0,0 +1,27 @@
+from relationships.modify import FedoraObject
+import argparse
+import yaml
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Switch pages to large images or books to compound objects')
+    pid = parser.add_argument("-p", "--pid", dest="pid", help="The PID you want to modify", required=True)
+    model = parser.add_argument("-m", "--model", dest="model", help="The destination model: compound, large_image", required=True)
+    args = parser.parse_args()
+    settings = yaml.safe_load(open("config.yml", "r"))
+    if args.model == 'compound':
+        FedoraObject(
+            settings['fedora_url'],
+            auth=(
+                settings['username'], settings['password']
+            )
+        ).convert_book_to_compound_object(args.pid)
+    elif args.model == 'large_image':
+        FedoraObject(
+            settings['fedora_url'],
+            auth=(
+                settings['username'], settings['password']
+            )
+        ).convert_page_to_part_of_compound_object(settings.pid)
+    else:
+        print(f'Operation not valid. Unknown model: {args.model}')
